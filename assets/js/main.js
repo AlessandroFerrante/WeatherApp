a16933078594c74e8259fde7504efa2e81684dbd
Alessandro Ferrante <104135680+AlessandroFerrante@users.noreply.github.com>
2023-10-19 20:37:10 +0200

    search limit added

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,12 @@ const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weather-details');
 const error404 = document.querySelector('.error404');
 
+let count = 0; // Initialize the counter to zero
+
+
+
 search.addEventListener('click', ()=>{
-    const apiKey= import.meta.env.VITE_API_KEY;
+    const apiKey='76f83619bb3ad1220c5c31ec99cd60b7';
     const city = document.querySelector('.search-box input').value; 
     
     if(city === ''){
@@ -19,10 +23,18 @@ search.addEventListener('click', ()=>{
         return;
     }
 
-        const apiUrl= import.meta.env.VITE_API_URL+`?q=${city}&units=metric&appid=${apiKey}`;
+        const apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
     fetch(apiUrl).then(response => response.json()).then(json => {
 
+        if (count < 20) {
+            document.getElementById("messageBox").innerHTML = "Search performed. You have " + (19 - count) + " searches available.";
+            count++;
+        }else {
+            alert("You have reached the limit of 5 clicks.");
+            document.getElementById("messageBox").innerHTML = "You have reached the limit of 20 searches.";
+            document.getElementById("search-box button").disabled = true;
+        }
         if(json.cod === '404'){
             container.style.height= '400px';
             weatherBox.style.display = 'none';
